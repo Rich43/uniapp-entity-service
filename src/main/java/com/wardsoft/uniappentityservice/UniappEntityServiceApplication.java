09e5f209291ba69bd94afb1d53fbc57db6acbf9d
Richard Ward <RichieS@GMail.com>
2022-05-29 10:56:58 +0100

    Initial commit

---
+++ src/main/java/com/wardsoft/uniappentityservice/UniappEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wardsoft.uniappentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniappEntityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniappEntityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wardsoft/uniappentityservice/UniappEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wardsoft.uniappentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniappEntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
